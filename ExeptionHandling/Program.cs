6aaf6da95b014a6e6cfbee30ceec40cab93600b3
Dileep Singh <44288066+DsR90@users.noreply.github.com>
2019-04-03 12:55:43 +0530

    Add project files.

---
+++ ExeptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int divisor = 0;
+            try
+            {
+                
+                int result = num / divisor;
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally block");              
+
+            }
+            Console.WriteLine("After Finally Block");
+            Console.ReadKey();
+        }
+    }
+}
